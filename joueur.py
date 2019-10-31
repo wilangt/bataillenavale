6b253b85e6c0bbd431d3eac30e5d33954911321e
wilangt <wilangt>
2019-10-31 22:51:36 +0100

    Ajout doc + template pour la classe Joueur

--- joueur.py
+++ joueur.py
@@ -11,9 +11,11 @@ class Joueur:
         self.attaquant = False
 
     def position_bateaux(self):
+        """Fonction qui renvoie la position des bateaux sous forme d'une liste. A completer ou a supprimer"""
         raise NameError("Erreur, pas de fonction placerBateaux")
 
     def placer_bateaux(self):
+        """Fonction qui place les bateaux sur le plateau. A ne pas toucher"""
         bateaux = self.position_bateaux()
         if test_bateaux(bateaux):
             self.plateau_allie.placer_bateaux(bateaux)
@@ -21,16 +23,44 @@ class Joueur:
             raise NameError("Erreur, les bateaux n'ont pas été placés correctement")
 
     def choisir_cible(self):
+        """Fonction qui étant donné une configuration, choisi une cible et la renvoie sous forme de coordonnées.
+        A completer ou a supprimer"""
         raise NameError("Erreur, pas de fonction choisirCible")
 
     def attaquer(self):
+        """Fonction qui ouvre le feu sur une position ennemie. A ne pas toucher"""
         self.analyser(self.plateau_adverse.feu(self.choisir_cible()))
 
     def analyser(self, resultat):
+        """Fonction qui analyse la réponse du tir. A completer ou a supprimer"""
         pass
 
     def est_defenseur(self):
+        """Fonction qui permet de savoir si le joueur peut défendre"""
         return self.defenseur
 
     def est_attaquant(self):
+        """Fonction qui permet de savoir si le joueur peut attaquer"""
         return self.attaquant
+
+"""
+Template pour sous-classes de joueur :
+class Humain(Joueur):
+    def __init__(self, plateau_allie, plateau_adverse):
+        Joueur.__init__(self, plateau_allie, plateau_adverse)
+        self.attaquant = False
+        self.defenseur = False
+
+    def position_bateaux(self):
+        pass 
+        # ICI remplir ou supprimer
+
+    def choisir_cible(self):
+        pass
+        # ICI remplir ou supprimer
+
+    def analyser(self, res):
+        pass
+        # ICI remplir ou supprimer
+
+"""
