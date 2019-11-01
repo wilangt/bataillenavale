48f39adb9d65043d0a8d64c867d9ecb7926a15ea
wilangt <wilangt>
2019-11-01 14:59:20 +0100

    Défenseur humain replace les bateaux jusqu'à configuration valide

--- humain.py
+++ humain.py
@@ -1,6 +1,7 @@
 from plateau import *
 from joueur import *
 from pygame.locals import *
+from fonctions_annexes import test_bateaux
 
 
 class Humain(Joueur):
@@ -10,44 +11,43 @@ class Humain(Joueur):
         self.defenseur = True
 
     def position_bateaux(self):
-        self.plateau_allie.init_interface(500)
-        self.plateau_allie.afficher_interface()
-        pas_quad = self.plateau_allie.get_pas_quad()
-        continuer = True
-        pygame.event.clear()
-
         liste_bateaux = [[], [], [], [], []]
-        taille_bateaux = [2, 3, 3, 4, 5]
-        print("2 cases")
-        bateau_actuel = 0
-        compteur = 0
-
-        while continuer:
-            for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
-                if event.type == QUIT:  # Si un de ces événements est de type QUIT
-                    pygame.display.quit()
-                    pygame.quit()
-                    raise NameError("Abandon")
-
-                if event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[1] > pas_quad and event.pos[
-                        0] > pas_quad:
-                    coor = pix_to_coor(event.pos[0], event.pos[1], pas_quad)
-                    liste_bateaux[bateau_actuel].append(coor)
-                    self.plateau_allie.afficher_rectangle(coor, "blanc")
-                    compteur += 1
-                    if compteur >= taille_bateaux[bateau_actuel]:
-                        liste_bateaux[bateau_actuel].sort()
-                        compteur = 0
-                        bateau_actuel += 1
-                        if bateau_actuel <= 4:
-                            print("{} cases".format(taille_bateaux[bateau_actuel]))
-                        else:
-                            print("Bateaux placés")
-                    if bateau_actuel >= 5:
-                        continuer = False
-                        self.plateau_allie.cacher_interface()
-                    #print(liste_bateaux)
-
+        while not test_bateaux(liste_bateaux):
+            self.plateau_allie.init_interface(500)
+            self.plateau_allie.afficher_interface()
+            pas_quad = self.plateau_allie.get_pas_quad()
+            continuer = True
+            pygame.event.clear()
+            taille_bateaux = [2, 3, 3, 4, 5]
+            print("2 cases")
+            bateau_actuel = 0
+            compteur = 0
+            while continuer:
+                for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
+                    if event.type == QUIT:  # Si un de ces événements est de type QUIT
+                        pygame.display.quit()
+                        pygame.quit()
+                        raise NameError("Abandon")
+                    if event.type == MOUSEBUTTONDOWN and event.button == 1 and event.pos[1] > pas_quad and event.pos[
+                            0] > pas_quad:
+                        coor = pix_to_coor(event.pos[0], event.pos[1], pas_quad)
+                        liste_bateaux[bateau_actuel].append(coor)
+                        self.plateau_allie.afficher_rectangle(coor, "blanc")
+                        compteur += 1
+                        if compteur >= taille_bateaux[bateau_actuel]:
+                            liste_bateaux[bateau_actuel].sort()
+                            compteur = 0
+                            bateau_actuel += 1
+                            if bateau_actuel <= 4:
+                                print("{} cases".format(taille_bateaux[bateau_actuel]))
+                        if bateau_actuel >= 5:
+                            continuer = False
+                            if test_bateaux(liste_bateaux):
+                                print("Bateaux placés")
+                            else:
+                                print("erreur, bateaux mal placés, veuillez recommencer")
+                            self.plateau_allie.cacher_interface()
+            # print(liste_bateaux)
         return liste_bateaux
 
     def choisir_cible(self):
